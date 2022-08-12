063c60f2c3c99a3958a27e1d2dcfaf19f24a51d9
vedatgorucu <109813745+vedatgorucu@users.noreply.github.com>
2022-08-12 15:45:38 +0300

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-echo "Hello world"
+echo "Tayfun/Hasan naber cnm"
+read -p "YES/NO: " NABER
+echo 'export NODENAME='$NABER >> $HOME/.bash_profile
+if [  $WALLET == "YES" ]; then
+	echo "ALLAH İYİLİK VERSİN CANIM"
+fi
+if [  $WALLET == "NO" ]; then
+	echo "NEDEN KÖTÜSÜN ANLAT BROM"
+fi
