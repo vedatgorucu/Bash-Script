401ccd6adf89eb10b2ecbabd49631b355767ca44
vedatgorucu <109813745+vedatgorucu@users.noreply.github.com>
2022-08-12 15:54:27 +0300

    Update first.sh

--- first.sh
+++ first.sh
@@ -2,9 +2,9 @@
 echo "Tayfun/Hasan naber cnm"
 read -p "YES/NO: " NABER
 echo 'export NABER='$NABER >> $HOME/.bash_profile
-if [  "$NABER" == "YES" ]; then
+if [[  "$NABER" == "YES" ]]; then
 	echo "ALLAH İYİLİK VERSİN CANIM"
 fi
-if [  "$NABER" == "NO" ]; then
+if [[  "$NABER" == "NO" ]]; then
 	echo "NEDEN KÖTÜSÜN ANLAT BROM"
 fi
